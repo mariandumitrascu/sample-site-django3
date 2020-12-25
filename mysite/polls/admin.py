6e5296dbdeee276c8bac9993e7b96c0b92e69aaa
Marian Dumitrascu <md@dumitrascu.net>
2020-12-25 12:31:02 -0600

    changes to polls admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
+# this tells django Choice objects are edited on the Question admin page
+# by default provide enough fields for 3 choices
+# reference for django.contrib.admin:
+# https://docs.djangoproject.com/en/1.8/_modules/django/contrib/admin/options/
+class ChoiceInline(admin.StackedInline):
+# class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -11,9 +19,28 @@ class QuestionAdmin(admin.ModelAdmin):
                 'fields': ['question_text']
             }
         ),
-        ('Date information',        {'fields': ['pub_date']         }),
+
+        (
+            'Date information',
+            {
+                'fields': ['pub_date']
+            }
+        ),
     ]
 
+    # this makes the choices to be editable on same page with the Question
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']  # this is adding a search box
+    show_full_result_count = True
+    # view_on_site = True
 
+    # reference: https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_per_page
+    list_per_page = 2
+
+
+# this makes to appear in the admin menu
+# QuestionAdmin makes those separator bars between field sets to show
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+# admin.site.register(Choice)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,6 +15,14 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    # a filed coming from a method cannot be ordered
+    # by default the heading of the field in the table shows the name of the method
+    # thats why we set .short_description
+    # reference on thse type of fields:
+    # https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_display
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
